c99d5999ad2b39435a81127fea753506dab92fa5
tfiordalisi <tomas.fiordalisi@mercadolibre.com>
2019-09-06 11:44:30 -0300

    Added some changes

--- src/com/unlam/triangulo/Triangulo.java
+++ src/com/unlam/triangulo/Triangulo.java
@@ -1,33 +1,33 @@
 package com.unlam.triangulo;
 
 public class Triangulo {
-    private int l1, l2, l3;
+    private double l1, l2, l3;
 
-    public Triangulo(int l1, int l2, int l3) throws Exception {
-        if((l1 < 1 || l2 < 1 || l3 < 1) || (l1 + l2 < l3 || l2 + l3 < l1 || l3 + l1 < l2)) {
-            throw new Exception("Noooo, eso no e triaaaangulo papi");
+    public Triangulo(double l1, double l2, double l3) throws Exception {
+        if((l1 < 0 || l2 < 0 || l3 < 0) || (l1 + l2 < l3 || l2 + l3 < l1 || l3 + l1 < l2)) {
+            throw new Exception("Error: No es un triangulo");
         }
         this.l1 = l1;
         this.l2 = l2;
         this.l3 = l3;
     }
 
-    public int getL1() {
+    public double getL1() {
         return l1;
     }
 
-    public int getL2() {
+    public double getL2() {
         return l2;
     }
 
-    public int getL3() {
+    public double getL3() {
         return l3;
     }
 
     public TipoTriangulo obtenerTipoTriangulo() {
         if (this.getL1() == this.getL2() && this.getL2() == this.getL3()) {
             return TipoTriangulo.EQUILATERO;
-        } else if (this.getL1() != this.getL2() && this.getL2() != this.getL3() && this.getL1() != this.getL2()) {
+        } else if (this.getL1() != this.getL2() && this.getL2() != this.getL3() && this.getL1() != this.getL3()) {
             return TipoTriangulo.ESCALENO;
         } else {
             return TipoTriangulo.ISOSCELES;
--- src/test/triangulo/TrianguloTest.java
+++ src/test/triangulo/TrianguloTest.java
@@ -37,12 +37,65 @@ public class TrianguloTest {
 
     @Test(expected = Exception.class)
     public void exceptionNoEsTrianguloTest() throws Exception {
-        Triangulo t4 = new Triangulo(1, 2, 60);
+        Triangulo t4 = new Triangulo(14, 14, 29);
     }
 
     @Test(expected = Exception.class)
     public void exceptionLadoNegativoTest() throws Exception {
         Triangulo t5 = new Triangulo(1, 2, -3);
     }
+
+    @Test(expected = Exception.class)
+    public void dosLadosMenorQueElTercero() throws Exception {
+        Triangulo t5 = new Triangulo(1,1,3);
+        Triangulo t6 = new Triangulo(90,172,24);
+        Triangulo t7 = new Triangulo(2,11,14);
+        Triangulo t8 = new Triangulo(14,14,29);
+        Triangulo t9 = new Triangulo(23,11,11);
+        Triangulo t10 = new Triangulo(19,44,19);
+    }
+
+    @Test(expected = Exception.class)
+    public void valoresNoEnterosConDosLadosMenorQueElTercero() throws Exception{
+        Triangulo t5 = new Triangulo(1.1,1.5,1.03);
+        Assert.assertEquals(TipoTriangulo.ESCALENO, t5.obtenerTipoTriangulo());
+        Triangulo t1 = new Triangulo(0.23203,334.12,10.0000);
+    }
+
+    @Test
+    public void valoresNoEnteros() throws Exception {
+        Triangulo t5 = new Triangulo(13.312,13.312,14.202);
+        Assert.assertEquals(TipoTriangulo.ISOSCELES, t5.obtenerTipoTriangulo());
+        Triangulo t6 = new Triangulo(0.2,0.4,0.3);
+        Assert.assertEquals(TipoTriangulo.ESCALENO, t6.obtenerTipoTriangulo());
+        Triangulo t7 = new Triangulo(120.253,120.253,120.253);
+        Assert.assertEquals(TipoTriangulo.EQUILATERO, t7.obtenerTipoTriangulo());
+        Triangulo t8 = new Triangulo(31.345,313.45,300.6);
+        Assert.assertEquals(TipoTriangulo.ESCALENO, t8.obtenerTipoTriangulo());
+
+    }
+
+    @Test
+    public void EnterosYDecimales() throws Exception {
+        Triangulo t3 = new Triangulo(12.012,12,12.00012);
+        Assert.assertEquals(TipoTriangulo.ESCALENO, t3.obtenerTipoTriangulo());
+        Triangulo t4 = new Triangulo(5,5.5,5.55);
+        Assert.assertEquals(TipoTriangulo.ESCALENO, t4.obtenerTipoTriangulo());
+        Triangulo t5 = new Triangulo(4,0.21d,4);
+        Assert.assertEquals(TipoTriangulo.ISOSCELES, t5.obtenerTipoTriangulo());
+        Triangulo t6 = new Triangulo(13.3,22,12.12);
+        Assert.assertEquals(TipoTriangulo.ESCALENO, t6.obtenerTipoTriangulo());
+        Triangulo t7 = new Triangulo(123.2,123.2,76);
+        Assert.assertEquals(TipoTriangulo.ISOSCELES, t7.obtenerTipoTriangulo());
+        Triangulo t8 = new Triangulo(41.8989,41.8989,41.8989);
+        Assert.assertEquals(TipoTriangulo.EQUILATERO, t8.obtenerTipoTriangulo());
+        Triangulo t9 = new Triangulo(6.123,6.122,6.1233);
+        Assert.assertEquals(TipoTriangulo.ESCALENO, t9.obtenerTipoTriangulo());
+        Triangulo t10 = new Triangulo(80,80.006,80);
+        Assert.assertEquals(TipoTriangulo.ISOSCELES, t10.obtenerTipoTriangulo());
+
+    }
+
+
 
 }
